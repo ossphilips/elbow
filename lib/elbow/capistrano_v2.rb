7fc5bd6ba19819cc83cbbce5521cebf3f0c033c3
Rian Wouters <Rian.Wouters@philips.com>
2014-07-21 14:52:33 +0200

    Refactor elb name resolution

--- lib/elbow/capistrano_v2.rb
+++ lib/elbow/capistrano_v2.rb
@@ -4,19 +4,13 @@ require 'net/dns'
 Capistrano::Configuration.instance(:must_exist).load do
 
   def elastic_load_balancer(name, *args)
-
-    packet = Net::DNS::Resolver.start(name)
-    all_cnames= packet.answer.reject { |p| !p.instance_of? Net::DNS::RR::CNAME }
-    cname = all_cnames.find { |c| c.name == "#{name}."}
-    elb_name = cname ? cname.cname[0..-2].downcase : name
-
     aws_region= fetch(:aws_region, 'us-east-1')
     AWS.config(:access_key_id => fetch(:aws_access_key_id),
                :secret_access_key => fetch(:aws_secret_access_key),
                :ec2_endpoint => "ec2.#{aws_region}.amazonaws.com",
                :elb_endpoint => "elasticloadbalancing.#{aws_region}.amazonaws.com")
 
-    load_balancer = AWS::ELB.new.load_balancers.find { |elb| elb.dns_name.downcase == elb_name }
+    load_balancer = AWS::ELB.new.load_balancers.find { |elb| elb.dns_name.downcase == elb_name(name) }
     raise "EC2 Load Balancer not found for #{name} in region #{aws_region}" if load_balancer.nil?
 
     hostnames = load_balancer.instances.collect do |instance|
@@ -36,4 +30,10 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
   end
 
+  def elb_name(name)
+    packet = Net::DNS::Resolver.start(name)
+    all_cnames= packet.answer.reject { |p| !p.instance_of? Net::DNS::RR::CNAME }
+    cname = all_cnames.find { |c| c.name == "#{name}."}
+    cname ? cname.cname[0..-2].downcase : name
+  end
 end
